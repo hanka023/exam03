9e5f26282301980612d1ad085a3c34b58ab7f002
hanka023 <hankaskalova02@gmail.com>
2026-04-03 19:40:21 +0200

    buggy buggy

---
+++ rip.c
@@ -0,0 +1,201 @@
+// Assignment name: rip
+// Expected files: *.c *.h
+// Allowed functions: puts, write
+// --------------------------------------------------------------------------------
+// Write a program that will take as argument a string containing only parenthesis.
+// If parenthesis are unbalanced (for example "())") your program shall remove the
+// minimum number of parenthesis for the expression to be balanced.
+// By removing we mean replacing by spaces.
+// You will print all the solutions (can be more than one).
+// The order of the solutions is not important.
+// For example this should work:
+// (For readability reasons the '_' means space and the spaces are for readability only.)
+// $> ./rip '( ( )' | cat -e
+// _ ( ) $
+// ( _ ) $
+// $> ./rip '( ( ( ) ( ) ( ) ) ( ) )' | cat -e
+// ( ( ( ) ( ) ( ) ) ( ) ) $
+// $> ./rip '( ) ( ) ) ( )' | cat -e
+// ( ) ( ) _ ( ) $
+// ( ) ( _ ) ( ) $
+// ( _ ( ) ) ( ) $
+// $> ./rip '( ( ) ( ( ) (' | cat -e
+// ( ( ) _ _ ) _ $
+// ( _ ) ( _ ) _ $
+// ( _ ) _ ( ) _ $
+// _ ( ) ( _ ) _ $
+// _ ( ) _ ( ) _ $
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ft_strlen(char *str)
+{
+    int i = 0;
+    while (*str)
+    {
+        ++i;
+        ++str;
+    }
+    return (i);
+}
+
+char *array(char *str)
+{
+    char *start;
+    int len = ft_strlen(str);
+    char *s = malloc(sizeof (char) * (len + 1));
+    if (!s)
+        return (0);
+    s[len] = '\0';
+    start = s;
+
+    while (*str)
+    {
+        *s = *str;
+        ++s;
+        ++str;
+    }
+    s = start;
+    return (s);
+}
+
+
+int diff(char *str)
+{
+    int i = 0; 
+  
+    while(*str)
+    {
+        if (*str == '(')
+            ++i;
+        else if (*str == ')')
+            --i;
+        ++str;
+    }
+  return (i);
+}
+
+int parity(char *str)
+{
+    int left = 0; 
+    int right = 0;
+    int diff = 0;
+
+    while(*str)
+    {
+        if (*str == '(')
+                ++left;
+        else if (*str == ')')
+        {   if (left <= 0)
+                 return (0);
+            --left;
+        }
+        ++str;
+    }
+    if (left == right)
+        return (1);
+    else 
+        return (0);
+}
+
+
+void backtrack(char *str, int dif, int len, int i)
+{
+    
+    int par = parity(str); 
+    char c;
+
+    if(parity (str))
+        printf ("zavorky %s\n",str );
+
+    if (i == len)
+        return;
+
+    // 2. MOŽNOST: Znak necháme tak, jak je
+    backtrack(str, dif, len, i+ 1);
+
+
+    while (dif > 0)
+    {
+        c = str[i]; 
+        str[i] = '_';
+        backtrack(str, dif - 1, len, i+1);
+        str[i] = c;
+    }
+}
+
+void pars(char *str)
+{
+    int left = 0; 
+    int right = 0;
+    int i;
+    char *start = str;
+    int len = strlen(str);
+ 
+
+    int dif = diff(str);  //kolik zavorek smazu
+    int par = parity(str);  //jestli jsou uzavrene a muzu tisknout
+    i = 0;
+    backtrack(str, dif, len, i);
+
+    while (*str)
+    {
+        if (*str == '(')
+            ++left;
+        else if (*str == ')')
+            ++right;
+        ++str;
+    }
+
+    str = start;
+    i = 0;
+
+    if (left > right)
+    {
+        while (str[i] != '\0' && (left > right))
+        {
+            if ( str[i] == '(')
+            {
+                str[i] = '_';
+                --left;
+            }
+            ++i;
+        }
+    }
+    else if (left < right)
+    {
+        while (str[i] != '\0' && (left < right))
+        {
+            if (str[i] == ')')
+            {
+                str[i] = '_';
+                --right;
+            }
+            ++i;
+        }
+    }
+    str = start;
+    //printf("%s\n", str);
+    return;
+}
+
+int main (int argc, char *argv[])
+{
+
+    if (argc != 2)
+        return (0);
+
+    char *str = array(argv[1]);
+
+    pars(str);
+
+    free(str);
+    return (0);
+}
+
+
+
+
